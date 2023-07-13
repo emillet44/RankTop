643d72a4764fc017ad74c7916c217cdab11bf80e
emillet44 <evan.s.millet@gmail.com>
2023-07-13 14:37:00 -0500

    update

--- app/newpost/page.tsx
+++ app/newpost/page.tsx
@@ -1,38 +1,91 @@
-import { faXmark } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import Link from 'next/link'
+'use client'
 
-export default function Post() {
+import { newList } from '../listupload'
+import Link from 'next/link'
+import React, { useState } from "react";
+
+export default function NewPost() {
+  
+  const [selected, setSelected] = useState("");
+  const [exptoggle, setexp] = useState("");
+
+  const getInput = (e: any) => {
+    setSelected(e.target.value);
+  };
+
+  const toggleExp = (e: any) => {
+    e.preventDefault();
+    if(e.target.textContent == "Add Explanation") {
+      e.target.textContent = "Remove Explanation";
+      setexp(e.target.textContent);
+    }
+    else {
+      e.target.textContent = "Add Explanation";
+      setexp(e.target.textContent);
+    }
+  }
+
   return (
     <>
       <div className="flex justify-center py-7 bg-slate-500">
         <Link href="/">
           <button className="absolute left-2 top-3 text-4xl/7">DIGBTT</button>
         </Link>
-        <Link href="/signin">
-          <button className="absolute right-2 top-1.5 hover:outline outline-2 py-2 px-2 rounded-sm">Sign In</button>
+        <Link href="/api/auth/signin">
+          <button className="absolute right-2 top-1.5 hover:outline outline-2 p-2 rounded-sm">Sign In</button>
         </Link>
       </div>
 
-      <div className="flex justify-center">
-        <form method="post" className="py-20 grid grid-cols-1 grid-rows-8 gap-10 w-full max-w-3xl px-6">
-            <header className="text-3xl justify-self-left ">New Post</header>
-            <input type="text" placeholder="List Title" className="outline outline-2 justify-self-center rounded-sm w-full h-9 pl-2"></input>
-            <input type="text" placeholder="Rank #1" className="outline outline-2 justify-self-center rounded-sm h-9 w-full pl-2"></input>
-            <input type="text" placeholder="Rank #2" className="outline outline-2 justify-self-center rounded-sm h-9 w-full pl-2"></input>
-            <input type="text" placeholder="Rank #3" className="outline outline-2 justify-self-center rounded-sm h-9 w-full pl-2"></input>
-            <input type="text" placeholder="Rank #4" className="outline outline-2 justify-self-center rounded-sm h-9 w-full pl-2"></input>
-            <input type="text" placeholder="Rank #5" className="outline outline-2 justify-self-center rounded-sm h-9 w-full pl-2"></input>
-            <div className="justify-self-end space-x-5">
-              <Link href="/preview" className="outline outline-2 rounded-md p-2">
-                <button type="submit">Preview</button>
-              </Link>
-              <Link href="/submitted" className="outline outline-2 rounded-md p-2">
-                <button type="submit">Submit</button>
-              </Link>
+      <form id="newpost" action={newList} method="POST" className="flex justify-center pt-24 px-6">
+        <div className="grid grid-cols-1 grid-flow-row auto-rows-auto gap-6 w-full max-w-2xl">
+          <header className="text-3xl justify-self-left">New Post</header>
+          <div className="grid grid-cols-1 grid-flow-row auto-rows-auto gap-6 p-10 rounded-xl outline">
+            <input name="title" placeholder="Title" className="text-4xl outline-none" required />
+            <div>
+              <label className="text-xl p-2">1.</label>
+              <input name="r1" className="text-xl outline-none p-2 focus:border-b border-neutral-950 w-11/12" required />
             </div>
-        </form>
-      </div>
+            <div>
+            <label className="text-xl p-2">2.</label>
+            <input name="r2" className="text-xl outline-none p-2 focus:border-b border-neutral-950 w-11/12" required />
+            </div>
+            {parseInt(selected) >= 3 &&
+            <div>
+              <label className="text-xl p-2">3.</label>
+              <input name="r3" className="text-xl outline-none p-2 focus:border-b border-neutral-950 w-11/12" required />
+            </div>
+            }
+            {parseInt(selected) >= 4 &&
+            <div>
+              <label className="text-xl p-2">4.</label>
+              <input name="r4" className="text-xl outline-none p-2 focus:border-b border-neutral-950 w-11/12" required />
+            </div>
+            }
+            {selected === "5" &&
+            <div>
+              <label className="text-xl p-2">5.</label>
+              <input name="r5" className="text-xl outline-none p-2 focus:border-b border-neutral-950 w-11/12" required />
+            </div>
+            }
+          </div>
+          {exptoggle == "Remove Explanation" &&
+            <div>
+              <header className="text-3xl justify-self-left pb-6">Explanation</header>
+              <textarea name="explain" className="w-full max-w-2xl h-44 outline rounded-md p-5 focus:outline-4" required></textarea>
+            </div>
+          }
+          <div className="max-w-2xl w-full h-10 flex justify-end space-x-5">
+            <button onClick={toggleExp} className="outline outline-2 rounded-md p-2">Add Explanation</button>
+            <select onChange={getInput} className="p-2 outline outline-2 rounded-md">
+              <option value="2">2 Ranks</option>
+              <option value="3">3 Ranks</option>
+              <option value="4">4 Ranks</option>
+              <option value="5">5 Ranks</option>
+            </select>
+            <button type="submit" className="outline outline-2 rounded-md p-2">Submit</button>
+          </div>
+        </div>
+      </form>
     </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,75 @@
 import { faXmark } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Link from 'next/link'
+import prisma from '../lib/prisma'
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "./api/auth/[...nextauth]/route"
+import { Logout } from "./logout"
 
-export default function Home() {
+
+
+export default async function Home() {
+  let signedin = false;
+  const post = await prisma.post.findMany({
+    where: { verified: false},
+  });
+
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    signedin = true;
+  }
+
+
   return (
     <>
       <div className="flex justify-center pt-14 pb-4 md:py-2 bg-slate-500">
-        <form className="grid grid-flow-col outline outline-2 rounded-md p-1">
+        <div className="grid grid-flow-col outline outline-2 rounded-md p-1">
           <Link href="/">
             <button className="absolute left-2 top-3 text-4xl/7">DIGBTT</button>
           </Link>
-          <input type="text" placeholder="Search" className= "w-80 h-7 bg-slate-500 outline-none p-1"/>
-          <button>
+          <input type="text" placeholder="Search" className= "w-80 h-7 bg-slate-500 outline-none p-1 peer"/>
+          <button className="peer-placeholder-shown:invisible">
             <FontAwesomeIcon icon={faXmark} className="w-6 h-7"/>
           </button>
-          <Link href="/signin">
+          { !signedin &&
+          <Link href="/api/auth/signin">
             <button className="absolute right-2 top-1.5 hover:outline outline-2 py-2 px-2 rounded-sm">Sign In</button>
           </Link>
+          }
+          { !signedin &&
           <Link href="/newpost">
             <button className="absolute right-20 top-1.5 hover:outline outline-2 py-2 px-2 rounded-sm">New Post</button>
           </Link>
-        </form>
+          }
+          { signedin &&
+            <Logout />
+          }
+          { signedin &&
+            <Link href="/newpost">
+              <button className="absolute right-24 top-1.5 hover:outline outline-2 py-2 px-2 rounded-sm">New Post</button>
+            </Link>
+          }
+          
+        </div>
       </div>
 
-      <div className=" pt-20 px-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-10">
-        <button className="outline rounded-md h-52 w-50">
-          <form className="h-52 p-5 text-left">
-            <header className="capitalize text-2xl">best nvidia AIB partners</header>
-            <ul className="list-inside list-decimal">
-              <li className="capitalize">asus</li>
-              <li className="capitalize">gigabyte</li>
-              <li className="capitalize">mSI</li>
-            </ul>
-          </form>
-        </button>
-        <button className="outline rounded-md">List Title</button>
-        <button className="outline rounded-md">List Title</button>
-        <button className="outline rounded-md">List Title</button>
-        <button className="outline rounded-md">List Title</button>
+      <div className=" pt-20 px-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-10"> {post.map((list) => (
+        <div key={list.id}>
+          <button className="outline rounded-md h-52 w-full"> 
+            <form className="h-52 p-5 text-left">
+              <header className="capitalize text-2xl">{list.title}</header>
+                <ul className="list-inside list-decimal">
+                  <li className="capitalize">{list.rank1}</li>
+                  <li className="capitalize">{list.rank2}</li>
+                  <li className="capitalize empty:hidden">{list.rank3}</li>
+                  <li className="capitalize empty:hidden">{list.rank4}</li>
+                  <li className="capitalize empty:hidden">{list.rank5}</li>
+                </ul>
+            </form>
+          </button>
+        </div>
+        ))}
       </div>
     </>
     
